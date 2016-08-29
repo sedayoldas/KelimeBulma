93e48571283a10ff908ee35c48aceea9a4adba86
sedayoldas <sedaylds@windowslive.com>
2016-08-30 01:32:25 +0300

    KelimeBulma

---
+++ KelimeBulma/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace KelimeBulma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHarfVer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHarfVer
+            // 
+            this.btnHarfVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHarfVer.Location = new System.Drawing.Point(12, 109);
+            this.btnHarfVer.Name = "btnHarfVer";
+            this.btnHarfVer.Size = new System.Drawing.Size(73, 56);
+            this.btnHarfVer.TabIndex = 0;
+            this.btnHarfVer.Text = "Harf Aliym";
+            this.btnHarfVer.UseVisualStyleBackColor = true;
+            this.btnHarfVer.Click += new System.EventHandler(this.btnHarfVer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 363);
+            this.Controls.Add(this.btnHarfVer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHarfVer;
+
+    }
+}
+
---
+++ KelimeBulma/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KelimeBulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] kelimeler = { "masa", "kitap", "klavye", "monitör", "asparagas", "çekoslavakya" };
+        string kelime;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random gen = new Random();
+            int sayi = gen.Next(kelimeler.Length);
+            //sayi dizi içindeki bir kelimenin indexi
+            kelime = kelimeler[sayi];
+            int x = 12, y = 12;
+            for (int i = 0; i < kelime.Length; i++)//kelime içindeki harf sayısı kadar bir textbox grubu oluşturup forma eklenir
+            {
+                TextBox txt = new TextBox();
+                txt.Location = new System.Drawing.Point(x, y);
+                txt.Name = "textBox"+i;
+                txt.Size = new System.Drawing.Size(50, 50);
+                txt.TabIndex = 0;
+                txt.Font=new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+                x += 55;
+                this.Controls.Add(txt);
+            }
+            this.Text = kelime;
+        }
+
+        private void btnHarfVer_Click(object sender, EventArgs e)
+        {
+            Random gen = new Random();
+            ArrayList harfSirasi = new ArrayList();
+
+            int sayi = 0;
+
+            while (true)
+            {
+               sayi=  gen.Next(kelime.Length);//hangi harf gösterilecek
+            //kelime[sayi] şeklinde sadece 1 harf kullanıcıya gösterilecek
+               if (!harfSirasi.Contains(sayi))
+               {
+                   harfSirasi.Add(sayi);
+                   foreach (var item in this.Controls)
+                   {
+                       if (item is TextBox)
+                       {
+                           if (((TextBox)item).Name == "textBox" + sayi)
+                               ((TextBox)item).Text = kelime[sayi].ToString();
+                       }
+                   }
+                   break;
+               }
+            }
+
+
+        }
+    }
+}
